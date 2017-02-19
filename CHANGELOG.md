1d51dad096bf03132eae3aa76d8a45de72801f7f
Jógvan Magnus Haugaard Olsen <foeroyingur@gmail.com>
2017-02-19 23:11:22 +0100

    Bump version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,6 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/) and this p
 
 ## [Unreleased]
 
-## [0.1.0.dev1] - 2017-02-02
+## [0.1.0] - 2017-02-19
 ### Added
-- Initial development version
+- Initial version
--- setup.py
+++ setup.py
@@ -11,19 +11,19 @@ with open(path.join(root_dir, 'README.md'), encoding='utf-8') as readme:
 
 st.setup(name='PyFraME',
          version=version,
-         description='PyFraME: Python tool for Fragment-based Multiscale Embedding',
+         description='PyFraME: Python tools for Fragment-based Multiscale Embedding',
          long_description=long_description,
          url='https://gitlab.com/FraME-projects/PyFraME',
          author='Jógvan Magnus Haugaard Olsen',
          author_email='foeroyingur@gmail.com',
          license='GPLv3+',
-         classifiers=['Development Status :: 3 - Alpha',
+         classifiers=['Development Status :: 4 - Beta',
                       'Environment :: Console',
                       'Intended Audience :: Science/Research',
                       'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
                       'Natural Language :: English',
                       'Operating System :: POSIX :: Linux',
-                      'Programming Language :: Python :: 3.3',
+                      'Programming Language :: Python :: 3',
                       'Topic :: Scientific/Engineering :: Chemistry',
                       'Topic :: Scientific/Engineering :: Physics'
                       ],
@@ -31,5 +31,5 @@ st.setup(name='PyFraME',
          packages=['pyframe'],
          package_data={'pyframe': ['data/*.csv']},
          data_files=['VERSION'],
-         entry_points={'console_scripts': ['PyFraME = pyframe.__main__:main']}
+         #entry_points={'console_scripts': ['PyFraME = pyframe.__main__:main']}
          )
