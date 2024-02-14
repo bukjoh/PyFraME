83d5ce2cdd4cf801e187613b90089178b7ac6293
Jonas Vester <jonas-vester@hotmail.de>
2024-02-14 13:03:35 +0100

    Update induction_interactions and completely remove veloxchem dependency

--- pyframe/embedding/tests/test_induction_interactions.py
+++ pyframe/embedding/tests/test_induction_interactions.py
@@ -1,160 +1,114 @@
 """Tests PyFraME.embedding.subsystem.py"""
-import os
 import pytest
-import veloxchem as vlx
 import numpy as np
-import scipy
-from pyframe.embedding import subsystem, vlx_interface, induction_interactions, electrostatic_interactions, read_input
+from pyframe.embedding import induction_interactions
 
 
-def test_compute_induction_interation():
-    # Test H^{-} -(2AA) ENV|QM H -(0.74AA) H QM|ENV -(2AA) H^{-}
-    # static contributions
-    h2_xyz = """2
-        core H2                
-        H        0.0000000000     0.0000000000      0.0000000000
-        H        0.7399998775     0.0000000000      0.0000000000
-        """
-    basis = "sto-3g"
-    driver = vlx_interface.EmbeddingIntegralDriver(h2_xyz, basis)
-    h2, h_minus = read_input.reader(input_data=f'{os.path.dirname(__file__)}/data/two_atom_test.json')
-    electric_fields = h2.compute_electric_fields(coordinates=h_minus.coordinates, integral_drv=driver)
-    nuclear_fields = h2.compute_nuclear_fields(coordinates=h_minus.coordinates)
-    external_fields = electric_fields + nuclear_fields
-    h_minus.solve_induced_dipoles(external_fields=external_fields, threshold=1e-10)
-    ref_ind_dipole_1 = np.array([-0.039037184, 0., 0.])
-    ref_ind_dipole_2 = np.array([0.039037184, 0., 0.])
-    assert h_minus.induced_dipoles.induced_dipoles[0, 0] == pytest.approx(ref_ind_dipole_1[0], abs=1e-8)
-    assert h_minus.induced_dipoles.induced_dipoles[1, 0] == pytest.approx(ref_ind_dipole_2[0], abs=1e-8)
+def test_compute_induction_interaction(wat_wat,
+                                       wat_wat_h_ind,
+                                       wat_wat_ind_dip,
+                                       acrolein_wat,
+                                       acrolein_wat_h_ind,
+                                       acrolein_wat_ind_dip,
+                                       dummy_integral_driver_factory):
+    env_wat = wat_wat[1]
+    induced_dipoles_wat = wat_wat_ind_dip
+    driver = dummy_integral_driver_factory(wat_wat_h_ind)
+    h_ind = induction_interactions.compute_induction_interaction(induced_dipoles=induced_dipoles_wat,
+                                                                 coordinates=env_wat.coordinates,
+                                                                 integral_drv=driver)
+    assert h_ind.shape == (24, 24)
+    assert np.allclose(h_ind, wat_wat_h_ind)
+    assert isinstance(h_ind, np.ndarray)
+    env_acrolein = acrolein_wat[1]
+    induced_dipoles_acrolein = acrolein_wat_ind_dip
+    driver = dummy_integral_driver_factory(acrolein_wat_h_ind)
+    h_ind = induction_interactions.compute_induction_interaction(induced_dipoles=induced_dipoles_acrolein,
+                                                                 coordinates=env_acrolein.coordinates,
+                                                                 integral_drv=driver)
+    assert h_ind.shape == (24, 24)
+    assert np.allclose(h_ind, acrolein_wat_h_ind)
+    assert isinstance(h_ind, np.ndarray)
 
-    # echem test for induced dipoles
-    h2o_xyz = """3
-    water
-    O        0.0000000000      0.0000000000      0.0000000000                 
-    H        0.6891400000      0.8324710000      0.0000000000                 
-    H        0.7224340000     -0.8726890000      0.0000000000
-    """
-    molecule = vlx.Molecule.read_xyz_string(h2o_xyz)
-    basis = vlx.MolecularBasis.read(molecule, "cc-pvdz")
-    nocc = molecule.number_of_alpha_electrons()
-    V_nuc = molecule.nuclear_repulsion_energy()
-    # overlap
-    overlap_drv = vlx.OverlapIntegralsDriver()
-    S = overlap_drv.compute(molecule, basis).to_numpy()
-    # kinetic energy
-    kinetic_drv = vlx.KineticEnergyIntegralsDriver()
-    T = kinetic_drv.compute(molecule, basis).to_numpy()
-    # nuclear attraction
-    nucpot_drv = vlx.NuclearPotentialIntegralsDriver()
-    V = -1.0 * nucpot_drv.compute(molecule, basis).to_numpy()
-    # one-electron Hamiltonian
-    h = T + V
-    # two-electron Hamiltonian
-    eri_drv = vlx.ElectronRepulsionIntegralsDriver()
-    g = eri_drv.compute_in_memory(molecule, basis)
-    # initial guess
-    epsilon, C = scipy.linalg.eigh(h, S)
-    E_HF, C_HF = vlx_interface.scf_solver(h=h, V_nuc=V_nuc, C=C, nocc=nocc, g=g, S=S)
-    # define core and env
-    core, env = read_input.reader(input_data=f'{os.path.dirname(__file__)}/data/wat_in_wat_test.json')
-    driver = vlx_interface.EmbeddingIntegralDriver(h2o_xyz, "cc-pvdz")
-    # calculate nuclear es energy and electric fock matrix
-    e_nuc_es, f_el_es = electrostatic_interactions.compute_electrostatic_interaction(quantum_subsystem=core,
-                                                                                     classical_subsystem=env,
-                                                                                     integral_drv=driver)
-    E_s, C_s, ind_dip, e_ind, e_nuc_ind, e_mul_ind, e_el_ind = vlx_interface.scf_pe_solver(h=h + f_el_es,
-                                                                                           V_nuc=V_nuc + e_nuc_es,
-                                                                                           C=C_HF,
-                                                                                           nocc=nocc, g=g, S=S,
-                                                                                           embedding_driver=driver,
-                                                                                           core=core,
-                                                                                           env=env)
-    D = 2 * np.einsum("ik,jk->ij", C_s[:, :nocc], C_s[:, :nocc])
-    e_el_es = np.einsum("ab, ab", D, f_el_es)
-    e_pe_total = e_el_ind + e_el_es + e_mul_ind + e_nuc_ind + e_nuc_es
-    assert e_el_es == pytest.approx(0.006804775749414373, abs=1e-9)
-    assert e_nuc_es == pytest.approx(-0.08545956246530774, abs=1e-9)
-    assert e_ind == pytest.approx(-0.01006509940518837, abs=1e-9)
-    assert e_nuc_ind == pytest.approx(-0.006052391164318287, abs=1e-9)
-    assert e_mul_ind == pytest.approx(-0.0009277338262916619, abs=1e-9)
-    assert e_el_ind == pytest.approx(-0.003084974414578435, abs=1e-9)
-    assert e_pe_total == pytest.approx(-0.08871988612108175, abs=1e-9)
-    assert E_s == pytest.approx(-76.06472693147695, rel=1e-10)
-    # Test in comparison to dalton
-    # define molecule
-    acrolein_xyz = """8
-    C3OH4
-    C             -0.145335   -0.546770    0.000607
-    C              1.274009   -0.912471   -0.000167
-    C              1.630116   -2.207690   -0.000132
-    O             -0.560104    0.608977    0.000534
-    H             -0.871904   -1.386459    0.001253
-    H              2.004448   -0.101417   -0.000710
-    H              0.879028   -3.000685    0.000484
-    H              2.675323   -2.516779   -0.000673
-    """
 
-    # vlx_interface.xyz_bohr_to_angstrom(acrolein_xyz)
-    # return
-
-    molecule = vlx.Molecule.read_xyz_string(acrolein_xyz)
-    basis_str = "sto-3g"
-    basis = vlx.MolecularBasis.read(molecule, basis_str)
-
-    # better initial guess:
-    scf_drv = vlx.ScfRestrictedDriver()
-    scf_results = scf_drv.compute(molecule, basis)
-
-    # scf preparation
-    nocc = molecule.number_of_alpha_electrons()
-    V_nuc = molecule.nuclear_repulsion_energy()
-    # overlap
-    overlap_drv = vlx.OverlapIntegralsDriver()
-    S = overlap_drv.compute(molecule, basis).to_numpy()
-    # kinetic energy
-    kinetic_drv = vlx.KineticEnergyIntegralsDriver()
-    T = kinetic_drv.compute(molecule, basis).to_numpy()
-    # nuclear attraction
-    nucpot_drv = vlx.NuclearPotentialIntegralsDriver()
-    V = -1.0 * nucpot_drv.compute(molecule, basis).to_numpy()
-    # one-electron Hamiltonian
-    h = T + V
-    # two-electron Hamiltonian
-    eri_drv = vlx.ElectronRepulsionIntegralsDriver()
-    g = eri_drv.compute_in_memory(molecule, basis)
-    # initial guess -> does not converge here.
-    epsilon, C = scipy.linalg.eigh(h, S)
-    # therefore better guess from scf used here
-    C = scf_results['C']
-    E_HF, C_HF = vlx_interface.scf_solver(h=h, V_nuc=V_nuc, C=C, nocc=nocc, g=g, S=S)
-    # define core and env
-    #core_ac = subsystem.QuantumSubsystem(name="1x C3OH4",
-    #                                     input_data=f'{os.path.dirname(__file__)}/data/acrolein_wat_test.json')
-    #env_ac = subsystem.ClassicalSubsystem(name="2x H2O atoms + X on their bonds",
-    #                                      input_data=f'{os.path.dirname(__file__)}/data/acrolein_wat_test.json')
-
-    core_ac, env_ac = read_input.reader(input_data=f'{os.path.dirname(__file__)}/data/acrolein_wat_test.json')
-    driver = vlx_interface.EmbeddingIntegralDriver(acrolein_xyz, basis_str)
-    # calculate nuclear es energy and electric fock matrix
-    e_nuc_es, f_el_es = electrostatic_interactions.compute_electrostatic_interaction(quantum_subsystem=core_ac,
-                                                                                     classical_subsystem=env_ac,
-                                                                                     integral_drv=driver)
-    E_s, C_s, ind_dip, e_ind, e_nuc_ind, e_mul_ind, e_el_ind = vlx_interface.scf_pe_solver(h=h + f_el_es,
-                                                                                           V_nuc=V_nuc + e_nuc_es,
-                                                                                           C=C_HF,
-                                                                                           nocc=nocc, g=g, S=S,
-                                                                                           embedding_driver=driver,
-                                                                                           core=core_ac,
-                                                                                           env=env_ac)
-    D = 2 * np.einsum("ik,jk->ij", C_s[:, :nocc], C_s[:, :nocc])
-    e_el_es = np.einsum("ab, ab", D, f_el_es)
-    e_pe_total = e_el_ind + e_el_es + e_mul_ind + e_nuc_ind + e_nuc_es
-    assert e_el_es == pytest.approx(-0.281816699842, abs=1e-8)
-    assert e_nuc_es == pytest.approx(0.266706671106, abs=1e-8)
+def test_compute_induction_energy(wat_wat,
+                                  wat_wat_ind_dip,
+                                  acrolein_wat,
+                                  acrolein_wat_ind_dip
+                                  ):
+    env_wat = wat_wat[1]
+    induced_dipoles_wat = wat_wat_ind_dip
+    nuclear_fields = np.array([[-0.199083  , -0.47195685, -0.06765254],
+                               [-0.19851923, -0.20346855, -0.05862587],
+                               [-0.09091193, -0.30552953,  0.06793896],
+                               [ 0.1476691 , -0.03348313,  0.382633  ],
+                               [ 0.13236691, -0.06417787,  0.20803882],
+                               [ 0.29853772, -0.21678522,  0.84407775],
+                               [-0.27527766,  0.33289472,  0.09254776],
+                               [-0.16068392,  0.18100274,  0.11845155],
+                               [-0.14574911,  0.26154579, -0.00559795],
+                               [ 0.14652788,  0.03254194, -0.31427559],
+                               [ 0.37533683,  0.10413035, -0.86909412],
+                               [ 0.10517092, -0.05743362, -0.2370408 ]])
+    electric_fields = np.array([[ 0.19711192,  0.43265575,  0.05777407],
+                               [ 0.19064269,  0.19379572,  0.05348263],
+                               [ 0.09470435,  0.29246066, -0.06234231],
+                               [-0.14721846,  0.03552778, -0.39864684],
+                               [-0.13360034,  0.06732875, -0.21512317],
+                               [-0.29680713,  0.23716673, -0.88827068],
+                               [ 0.25810129, -0.30820227, -0.07922521],
+                               [ 0.15757219, -0.17490486, -0.10992855],
+                               [ 0.14221186, -0.24626534,  0.00491036],
+                               [-0.14723334, -0.03442903,  0.32690342],
+                               [-0.38115301, -0.11381754,  0.91989704],
+                               [-0.10469259,  0.06033117,  0.24452343]])
+    e_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_wat,
+                                                            total_fields=(nuclear_fields + electric_fields
+                                                                          + env_wat.multipole_fields))
+    e_nuc_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_wat,
+                                                                total_fields=nuclear_fields)
+    e_mul_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_wat,
+                                                                total_fields=env_wat.multipole_fields)
+    e_el_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_wat,
+                                                               total_fields=electric_fields)
+    assert e_ind == pytest.approx(-0.01006509940518837, abs=1e-8)
+    assert e_nuc_ind == pytest.approx(-0.006052391164318287, abs=1e-8)
+    assert e_mul_ind == pytest.approx(-0.0009277338262916619, abs=1e-8)
+    assert e_el_ind == pytest.approx(-0.003084974414578435, abs=1e-8)
+    assert isinstance(e_ind, float)
+    env_acrolein = acrolein_wat[1]
+    induced_dipoles_acrolein = acrolein_wat_ind_dip
+    nuclear_fields = np.array([[ 6.12879109e-01, -1.55417637e-02,  1.73067342e-04],
+                               [ 1.04301145e+00, -1.60325172e-01,  7.96009519e-05],
+                               [ 4.11031628e-01, -7.55901764e-02,  1.35767536e-04],
+                               [ 7.86367117e-01, -6.53785251e-02,  1.55895494e-04],
+                               [ 5.02156493e-01, -5.55531558e-02,  1.55060559e-04],
+                               [-3.19153416e-01, -5.93251403e-01,  2.22276122e-04],
+                               [-4.37631143e-01, -9.92073947e-01,  6.02737985e-04],
+                               [-1.66359890e-01, -4.25705483e-01, -4.61888783e-04],
+                               [-3.64444960e-01, -7.53873295e-01,  3.52231995e-04],
+                               [-2.31122829e-01, -5.02872399e-01, -2.33641946e-04]])
+    electric_fields = np.array([[-6.16582892e-01,  1.07395149e-02, -1.75560439e-04],
+                               [-1.05955087e+00,  1.51020748e-01, -8.55583977e-05],
+                               [-4.14238593e-01,  7.37808927e-02, -1.37740454e-04],
+                               [-7.93975504e-01,  5.85703570e-02, -1.60027257e-04],
+                               [-5.05833874e-01,  5.25414026e-02, -1.57389039e-04],
+                               [ 3.26147215e-01,  5.92319398e-01, -2.25213665e-04],
+                               [ 4.56788209e-01,  9.99796691e-01, -6.18164979e-04],
+                               [ 1.70121056e-01,  4.26930308e-01,  4.65778525e-04],
+                               [ 3.75860864e-01,  7.55273447e-01, -3.58332160e-04],
+                               [ 2.36323858e-01,  5.03507098e-01,  2.34844522e-04]])
+    e_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_acrolein,
+                                                            total_fields=(nuclear_fields + electric_fields
+                                                                          + env_acrolein.multipole_fields))
+    e_nuc_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_acrolein,
+                                                                total_fields=nuclear_fields)
+    e_mul_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_acrolein,
+                                                                total_fields=env_acrolein.multipole_fields)
+    e_el_ind = induction_interactions.compute_induction_energy(induced_dipoles=induced_dipoles_acrolein,
+                                                               total_fields=electric_fields)
     assert e_ind == pytest.approx(-0.000751929428, abs=1e-9)
     assert e_nuc_ind == pytest.approx(0.042124675086, abs=1e-8)
     assert e_mul_ind == pytest.approx(0.000114734186, abs=1e-10)
     assert e_el_ind == pytest.approx(-0.0429913387, abs=1e-8)
-    assert e_pe_total == pytest.approx(-0.015861958165, abs=1e-8)
-    assert E_s == pytest.approx(-188.314434428389, rel=1e-10)
-
+    assert isinstance(e_ind, float)
