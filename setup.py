4d2862abe8b833949ef40115e4d8bba177f84ae9
Jonas Vester <jonas-vester@hotmail.de>
2024-03-05 15:01:40 +0100

    Move setup info into cfg format

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-# This project is configured via setup.cfg
+from setuptools import Extension, setup
+# import numpy
+
+setup(
+    ext_modules=[
+        Extension(
+            name="pyframe.embedding.cpp_interaction_tensor_element",
+            sources=["pyframe/embedding/interaction_tensor_element.cpp"],
+            # include_dirs=[numpy.get_include()],
+        ),
+    ]
+)
