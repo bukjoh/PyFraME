f1f852b9a63b97d11dafaa16b5c8c21d2f123b17
Jonas Vester <jonas-vester@hotmail.de>
2024-03-12 16:05:13 +0100

    Change setup to test install on ubuntu

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ ext_modules = [Extension(name="pyframe.embedding.cpp_interaction_tensor_element"
                          sources=["pyframe/embedding/interaction_tensor_element.cpp"],
                          include_dirs=[np.get_include(), eigen_include_dir],  # Add eigen3 directory here
                          extra_compile_args=extra_compile_args,
+                         extra_link_args=extra_compile_args,
                          language="c++")]
 
 # Eigen version to download
