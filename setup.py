4deabc43dbd1c5ab1f59d5ae21b60a5032c70c94
Jonas Vester <jonas-vester@hotmail.de>
2024-03-12 15:06:10 +0100

    Change setup to test install on ubuntu

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ if platform.system() == 'Windows':
 else:
     extra_compile_args = ["-fopenmp"]
 
+extra_compile_args = ["/openmp"]
+
 # Determine Eigen include directory
 eigen_include_dir = os.environ.get('EIGEN_INCLUDE_DIR', 'eigen3')
 
@@ -28,27 +30,28 @@ EIGEN_VERSION = "3.3.9"
 # Define a custom build_ext command to download Eigen and include it in the build
 class CustomBuildExtCommand(build_ext):
     def run(self):
-        # Download Eigen if not already present
-        eigen_dir = os.path.join(self.build_lib, "eigen3")
-        eigen_url = f"https://gitlab.com/libeigen/eigen/-/archive/{EIGEN_VERSION}/eigen-{EIGEN_VERSION}.tar.gz"
-        eigen_tar_path = os.path.join(
-            self.build_temp, f"eigen-{EIGEN_VERSION}.tar.gz")
+        if 'EIGEN_INCLUDE_DIR' not in os.environ:
+            # Download Eigen if not already present
+            eigen_dir = os.path.join(self.build_lib, "eigen3")
+            eigen_url = f"https://gitlab.com/libeigen/eigen/-/archive/{EIGEN_VERSION}/eigen-{EIGEN_VERSION}.tar.gz"
+            eigen_tar_path = os.path.join(
+                self.build_temp, f"eigen-{EIGEN_VERSION}.tar.gz")
 
-        if not os.path.exists(eigen_dir):
-            print(f"Downloading Eigen {EIGEN_VERSION}...")
-            subprocess.run(["curl", "-L", eigen_url, "-o",
-                            eigen_tar_path], check=True)
+            if not os.path.exists(eigen_dir):
+                print(f"Downloading Eigen {EIGEN_VERSION}...")
+                subprocess.run(["curl", "-L", eigen_url, "-o",
+                                eigen_tar_path], check=True)
 
-            # Extract the downloaded tar.gz file
-            subprocess.run(["tar", "xz", "-C", self.build_temp,
-                            "-f", eigen_tar_path], check=True)
+                # Extract the downloaded tar.gz file
+                subprocess.run(["tar", "xz", "-C", self.build_temp,
+                                "-f", eigen_tar_path], check=True)
 
-            # Move the extracted Eigen directory to eigen_dir
-            extracted_eigen_dir = os.path.join(self.build_temp, f"eigen-{EIGEN_VERSION}")
-            os.rename(extracted_eigen_dir, eigen_dir)
+                # Move the extracted Eigen directory to eigen_dir
+                extracted_eigen_dir = os.path.join(self.build_temp, f"eigen-{EIGEN_VERSION}")
+                os.rename(extracted_eigen_dir, eigen_dir)
 
-        # Add the Eigen directory to include_dirs
-        self.include_dirs.append(eigen_dir)
+            # Add the Eigen directory to include_dirs
+            self.include_dirs.append(eigen_dir)
 
         # Continue with the build
         super().run()
