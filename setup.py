45acd7867ac1da407fbcc3d2e2463a9f5b7da22e
Jonas Vester <jonas-vester@hotmail.de>
2024-03-12 16:33:04 +0100

    Change setup to test install on ubuntu

--- setup.py
+++ setup.py
@@ -58,8 +58,6 @@ class CustomBuildExtCommand(build_ext):
 
 # Setup configuration
 setup(
-    #name="pyframe.embedding.cpp_interaction_tensor_element",
     cmdclass={"build_ext": CustomBuildExtCommand},
     ext_modules=ext_modules,
-    #install_requires=["numpy"],
 )
