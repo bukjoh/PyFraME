0093f37a9380c630cd1d9e7cec4422773254b67b
Jonas Vester <jonas-vester@hotmail.de>
2024-03-15 15:03:26 +0100

    Remove eigen3 download in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
 import os
-import subprocess
 import numpy as np
 import platform
 
@@ -19,47 +17,12 @@ ext_modules = [Extension(name="pyframe.embedding.engine",
                          sources=["pyframe/embedding/engine/engine.cpp",
                                   "pyframe/embedding/engine/computation.cpp",
                                   "pyframe/embedding/engine/global.cpp"],
-                         include_dirs=[np.get_include(), eigen_include_dir],  # Add eigen3 directory here
+                         include_dirs=[np.get_include(), eigen_include_dir],
                          extra_compile_args=extra_compile_args,
                          extra_link_args=extra_compile_args,
                          language="c++")]
 
-# Eigen version to download
-EIGEN_VERSION = "3.3.9"
-
-
-# Define a custom build_ext command to download Eigen and include it in the build
-class CustomBuildExtCommand(build_ext):
-    def run(self):
-        if 'EIGEN_INCLUDE_DIR' not in os.environ:
-            # Download Eigen if not already present
-            eigen_dir = os.path.join(self.build_lib, "eigen3")
-            eigen_url = f"https://gitlab.com/libeigen/eigen/-/archive/{EIGEN_VERSION}/eigen-{EIGEN_VERSION}.tar.gz"
-            eigen_tar_path = os.path.join(
-                self.build_temp, f"eigen-{EIGEN_VERSION}.tar.gz")
-
-            if not os.path.exists(eigen_dir):
-                print(f"Downloading Eigen {EIGEN_VERSION}...")
-                subprocess.run(["curl", "-v", "-L", eigen_url, "-o",
-                                eigen_tar_path], check=True)
-
-                # Extract the downloaded tar.gz file
-                subprocess.run(["tar", "xz", "-C", self.build_temp,
-                                "-f", eigen_tar_path], check=True)
-
-                # Move the extracted Eigen directory to eigen_dir
-                extracted_eigen_dir = os.path.join(self.build_temp, f"eigen-{EIGEN_VERSION}")
-                os.rename(extracted_eigen_dir, eigen_dir)
-
-            # Add the Eigen directory to include_dirs
-            self.include_dirs.append(eigen_dir)
-
-        # Continue with the build
-        super().run()
-
-
 # Setup configuration
 setup(
-    cmdclass={"build_ext": CustomBuildExtCommand},
     ext_modules=ext_modules,
 )
