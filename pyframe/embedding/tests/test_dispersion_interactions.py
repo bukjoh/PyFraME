8ff22909fb4c3c96d63a6b67373b98feb62c7d53
Jonas Vester <jonas-vester@hotmail.de>
2024-05-06 12:34:03 +0200

    Remove prints

--- pyframe/embedding/tests/test_dispersion_interactions.py
+++ pyframe/embedding/tests/test_dispersion_interactions.py
@@ -131,7 +131,6 @@ def test_compute_dispersion_interactions(two_oxygen,
                                                                         method='LJ',
                                                                         combination_rule='Lorentz-Berthelot',
                                                                         perturbation_cache=energy_props_g))
-    print(result_g)
     for i in range(3):
         assert pytest.approx(ref_contr[i], rel=1e-8) == result_g[perts_g.h][(i,)]
     result_gg = (dispersion_interactions.compute_dispersion_interactions(quantum_subsystem=core_neon,
@@ -157,8 +156,6 @@ def test_compute_dispersion_interactions(two_oxygen,
     for i in range(3):
         for j in range(i, 3):
             for k in range(j, 3):
-                # print(((i, j, k),))
-                # print(result_ggg[perts_ggg.h][(i, j, k)])
                 assert pytest.approx(ref_contr[counter], rel=1e-8) == result_ggg[perts_ggg.h][(i, j, k)]
                 counter += 1
     result_gggg = (dispersion_interactions.compute_dispersion_interactions(quantum_subsystem=core_neon,
