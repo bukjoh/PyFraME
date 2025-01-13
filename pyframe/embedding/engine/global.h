b2458a95ff34310ff216381600c24090f304b970
Jonas Vester <jonas-vester@hotmail.de>
2025-01-13 14:12:22 +0100

    Remove license scanning from gitlab ci

--- pyframe/embedding/engine/global.h
+++ pyframe/embedding/engine/global.h
@@ -16,8 +16,7 @@ extern std::vector<Eigen::MatrixXd> tensor_coefficients;
 extern Eigen::Matrix<Eigen::Matrix<int, 2, 3>, Eigen::Dynamic, Eigen::Dynamic> tensor_template_interaction;
 extern Eigen::Matrix<Eigen::Matrix<int, 2, 3>, Eigen::Dynamic, Eigen::Dynamic> tensor_template_potential;
 
-//global variables required for IndDipolesFields, need to be
-extern std::vector<Eigen::Vector3d> atom_coordinates;
+//global variables required for IndDipolesFiel
 extern Eigen::VectorXi indices;
 extern std::vector<std::unordered_set<int>> exclusions;
 extern Eigen::MatrixXd old_ind_dipoles;
